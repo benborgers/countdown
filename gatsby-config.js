74cfee96d9dbc1e979cecbc5e979f6b60784c40c
benborgers <borgersbenjamin@gmail.com>
2019-06-07 19:30:25 -0400

    PWA splash screen and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Countdown`,
+        short_name: `Countdown`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#c71f27`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      }
+    }
   ]
 }
