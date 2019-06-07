6e272ea9113dbd92032c876da82d71837ab85209
benborgers <borgersbenjamin@gmail.com>
2019-06-07 19:30:35 -0400

    PWA info in head

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,17 @@ export default () => (
   <React.Fragment>
     <Helmet>
       <title>Countdown</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+
+      <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+
+      <link href="/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="/splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
     </Helmet>
 
     <Global
