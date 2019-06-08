007aae424aa9f7edf47af36d2ae8b8996ef10678
benborgers <borgersbenjamin@gmail.com>
2019-06-07 22:14:28 -0400

    add apple-mobile-web-app-capable

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default () => (
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 
+      <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="white" />
 
       <link href="/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
